565d5ab6c2f7021cbf2c3383f3e7abf95d000205
mankaache <mankaache10@gmail.com>
2024-10-09 14:13:12 +0100

    initial colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':'#4285F4',
+        'secondary':'#1CE9B6',
+        'accent':'#00BFA5',
+        'light_accent':'#4FC3F7',
+        'title':'#3F4043',
+        'destructive':'#FF7370',
+        'light':'#ffffff'
+
+      }
+    },
   },
   plugins: [],
 }
