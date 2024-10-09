72f6a62f7f3a13a70c33145fe2e6ffdcc6857545
mankaache <mankaache10@gmail.com>
2024-10-09 14:14:43 +0100

    images

---
+++ src/assets/constants.js
@@ -0,0 +1,26 @@
+export const Navlinks = [
+    {
+        linkname:'Home',
+        href:'/'
+    },
+    {
+        linkname:'About Us',
+        href:'/about'
+    },
+    {
+        linkname:'Projects',
+        href:'/projects'
+    },
+    {
+        linkname:'Events',
+        href:'/events'
+    },
+    {
+        linkname:'Blog',
+        href:'/blog'
+    },
+    {
+        linkname:'Contact Us',
+        href:'/contact'
+    },
+]
---
+++ src/assets/images.js
@@ -0,0 +1,9 @@
+import Logo from '../../public/logo icon.svg'
+import Community from '../../public/community.jpg'
+import Organisers from '../../public/organisers.jpg'
+
+export{
+    Logo,
+    Community,
+    Organisers
+}
